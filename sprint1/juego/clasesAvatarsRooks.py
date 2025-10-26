4f82ed75fd54d9e286b3e4c842d7405ced547ac2
Andrés Emiliano Aguilar Méndez <a.aguilar.6@estudiantec.cr>
2025-10-26 14:19:12 -0600

    Base de clase Rooks
    Se define la base de las instancias de las diferentes torres

--- sprint1/juego/clasesAvatarsRooks.py
+++ sprint1/juego/clasesAvatarsRooks.py
@@ -1,8 +1,10 @@
+
+# imports para las clases de Avatars y Rooks
 import random
 
 class Avatars:
     def __init__(self):
-        # Definición de los 4 avatares
+        # Definición de los 4 avatars
         self.flechador = {
             "vida": 5,
             "daño": 1,
@@ -25,5 +27,33 @@ class Avatars:
             "vida": 25,
             "daño": 4,
             "duracion_aparicion": 20,
+            "duracion_ataque": random.randint(1, 5)
+        }
+
+class Rooks:
+    def __init__(self):
+        # Definición de los 4 tipos de torres
+        self.torreArena = {
+            "vida": 2,
+            "daño": 2,
+            "valor": 50,
+            "duracion_ataque": random.randint(1, 5) 
+        } 
+        self.torreRoca = {
+            "vida": 4,
+            "daño": 3,
+            "valor": 100,
+            "duracion_ataque": random.randint(1, 5)
+        }
+        self.torreFuego = {
+            "vida": 8,
+            "daño": 4,
+            "valor": 150,
+            "duracion_ataque": random.randint(1, 5)
+        }
+        self.torreAgua = {
+            "vida": 8,
+            "daño": 5,
+            "valor": 150,
             "duracion_ataque": random.randint(1, 5)
         }
